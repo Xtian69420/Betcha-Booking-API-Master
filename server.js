2a7bb68ecac7c58b0c597d663f14302f9e53368a
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2024-11-09 18:23:29 +0800

    Port edit

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server is running`);
 });
