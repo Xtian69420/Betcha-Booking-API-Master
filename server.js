75e073ac2032be47e1600e536d200f4d11bae586
Xtian69420 <signo.303167@fairview.sti.edu.ph>
2024-11-12 22:29:18 +0800

    add dotnev

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('./db'); 
 const path = require('path');
+require('dotenv').config();
 
 const userController = require('./controllers/userController');
 const unitController = require('./controllers/unitController');
