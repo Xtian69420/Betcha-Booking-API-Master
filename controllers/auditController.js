48ff2d4c4e3af6290202d564d335bee862ffa97f
Christian Signo <150314113+Xtian69420@users.noreply.github.com>
2024-12-02 14:42:51 +0800

    Update auditController.js

--- controllers/auditController.js
+++ controllers/auditController.js
@@ -4,7 +4,7 @@ exports.createAudit = async (req, res) => {
     try {
         const currentDate = new Date().toISOString();  
         
-        const lastAudit = await Audit.findOne().sort({ Reference });
+        const lastAudit = await Audit.findOne().sort({ Reference -1 });
 
         const newReference = lastAudit ? parseInt(lastAudit.Reference) + 1 : 1; 
 
